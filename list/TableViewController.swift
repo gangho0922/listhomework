0af3b5c076255f8e7d3ed90ac241738f11738879
AnnKangHo <duddnd6897@gmail.com>
2022-10-11 20:56:50 +0900

    check cell

--- list/TableViewController.swift
+++ list/TableViewController.swift
@@ -35,7 +35,7 @@ class TableViewController: UITableViewController {
 
         return cell
     }
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
--- list/memo.swift
+++ list/memo.swift
@@ -16,8 +16,30 @@ class list {
     }
     
     static var memoList = [
-        list(content: "kk"),
-        list(content: "kkk"),
-        list(content: "kkkk")
+        list(content: "first"),
+        list(content: "second"),
+        list(content: "third"),
+        list(content: "fourth"),
+        list(content: "fifth"),
+        list(content: "sixth"),
+        list(content: "seventh"),
+        list(content: "eighth"),
+        list(content: "ninth"),
+        list(content: "tenth"),
+        list(content: "eleventh"),
+        list(content: "twelveth"),
+        list(content: "thirteenth"),
+        list(content: "fourteenth"),
+        list(content: "fifteenth"),
+        list(content: "sixteenth"),
+        list(content: "seventeenth"),
+        list(content: "eighteenth"),
+        list(content: "nineteenth"),
+        list(content: "twentieth"),
+        list(content: "twenty one"),
+        list(content: "twenty two"),
+        list(content: "twenty three"),
+        list(content: "twenty four")
+        
     ]
 }
